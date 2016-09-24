fb752498a6d5d940585d023f1c9f3027386bca42
Heitor S. Carvalho <heitorc@cpqd.com.br>
2016-09-23 21:01:45 -0300

    [IA707] Correcting figures legend position

--- list1/plot_finess.py
+++ list1/plot_finess.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import glob 
 
 def main():
-    files = glob.glob('/home/heitor/Repo/IA707/list1/ex3/list1_ex3_fitness/fitness_evol*')
+    files = glob.glob('./ex3/list1_ex3_fitness/itens_ab/fitness_evol*')
     i = 1
     for file in files:
     	print 'Saving figure from file: ', file
@@ -18,7 +18,7 @@ def main():
         plt.setp(fit_plot1, linewidth=2)
         plt.setp(fit_plot2, linewidth=2)
         plt.setp(fit_plot3, linewidth=2)
-        plt.legend(loc=2)
+        plt.legend(loc=0)
         plt.axis([0, fitness.shape[0], 0, 4.5])
         plt.savefig('fitness_eval' + str(i))
         plt.clf()
